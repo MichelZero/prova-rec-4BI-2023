6bd1e5c994e473a3608c538e6d948131783c411b
Michel <106674127+MichelZero@users.noreply.github.com>
2023-12-06 16:19:50 -0300

    up

--- ex04.py
+++ ex04.py
@@ -1,9 +1,20 @@
 # autores: Cristiano e Michel
 # Data: 06/12/2023
 # Descrição: Exercício 04
-# Questão: Faça uma lista de compras de supermercado utilizando um dicionário.
-# A entrada dos produtos deve ser feita através de uma estrutura de repetição.
-# Calcule a média dos preços dos produtos.
+# Questão: 
+
+# Crie uma lista de compras de supermercado utilizando um 
+# dicionário. A entrada dos produtos será realizada por 
+# meio de uma estrutura de repetição. Além disso, você 
+# deverá calcular a média dos preços dos produtos. O usuário
+# deve informar a quantidade de produtos que deseja inserir.
+
+# Para completar este exercício, você deve criar um programa 
+# que permita ao usuário inserir produtos e seus respectivos 
+# preços em um dicionário. Utilize uma estrutura de repetição 
+# para facilitar a entrada de múltiplos produtos. 
+# Ao final, calcule a média dos preços dos produtos inseridos.
+
 
 produtos = {}
 total_precos = 0
